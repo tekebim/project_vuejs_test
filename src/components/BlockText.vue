59288b9fd2846ece6ca80e59b93874e8faad537b
Michael <michael@tekmedias.com>
2020-02-13 13:57:49 +0100

    add block text with slots areas to homepage

---
+++ src/components/BlockText.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>
+            <slot name="title"></slot>
+        </h1>
+        <p>
+            <slot name="content"></slot>
+        </p>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'BlockText',
+    }
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="home">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At est fugit impedit magni modi nostrum praesentium soluta ut veritatis voluptatem! Alias eius ipsum maxime molestias nemo possimus recusandae, repellat suscipit!</p>
+    <BlockText>
+      <template v-slot:title>Texte dans le Slot Label</template>
+      <template v-slot:content>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, distinctio fuga illum itaque nostrum perferendis quas quidem rerum vero voluptatibus! Aperiam, commodi culpa cum delectus deserunt doloribus enim incidunt inventore magni nesciunt nulla, optio, tempore veritatis! Animi blanditiis consequuntur hic illum odio sequi tempore ullam. A amet aut autem cum deserunt dicta distinctio dolores eveniet exercitationem expedita harum inventore labore laborum maiores minima minus molestiae nemo nihil nisi non optio porro praesentium quod quos recusandae repellat rerum sapiente sequi similique, sit soluta sunt tempora temporibus. Delectus dolorem eius eveniet ipsa ipsam non quasi, quod saepe vitae! Autem beatae nulla sapiente.</template>
+    </BlockText>
   </div>
 </template>
 
 <script>
+import BlockText from '@/components/BlockText.vue';
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    BlockText
+  }
 }
 </script>
 
