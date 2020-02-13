61a88799d3b73dfa980106e9fbd26bf88ef5f508
Michael <michael@tekmedias.com>
2020-02-13 12:10:43 +0100

    Add Navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,41 @@
 <template>
-  <div id="app">
-    <div class="container">
-    <div id="nav">
-      <router-link to="/">Home test</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <Navigation/>
+        <div class="container">
+            <router-view/>
+        </div>
     </div>
-    <router-view/>
-  </div>
-  </div>
 </template>
 
+<script>
+    import Navigation from '@/components/Navigation.vue'
+
+    export default {
+        components: {
+            Navigation
+        }
+    }
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-#nav {
-  padding: 30px;
-}
+    #nav {
+        padding: 30px;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav id="nav" class="navbar navbar-expand-lg static-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Project</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <router-link to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/about">About</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/services">Services</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navigation',
+    }
+</script>
+
+<style lang="scss" scoped>
+    #nav {
+        background-color: $primary-color;
+        .nav-item {
+            margin: 0 15px;
+            color: $light-color;
+        }
+    }
+</style>
